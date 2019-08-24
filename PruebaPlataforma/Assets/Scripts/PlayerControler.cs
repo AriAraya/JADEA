51b14de2f560c7deddd0db0500ff92ede59819b8
ArielAraya <ariel.arayac@gmail.com>
2019-08-24 07:29:58 -0600

    Un par de animaciones

--- PruebaPlataforma/Assets/Scripts/PlayerControler.cs
+++ PruebaPlataforma/Assets/Scripts/PlayerControler.cs
@@ -11,27 +11,41 @@ public class PlayerControler : MonoBehaviour
     void Update()
     {
         transform.rotation = Quaternion.identity;
-        if (Input.GetKey("left") )
+        if (Input.GetKey("down"))
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-7000f*Time.deltaTime,0));
-            gameObject.GetComponent<Animator>().SetBool("moving", true);
-            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            gameObject.GetComponent<Animator>().SetBool("agachandose", true);
         }
-        if (Input.GetKey("right"))
+        else
         {
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(7000f * Time.deltaTime, 0));
-            gameObject.GetComponent<Animator>().SetBool("moving", true);
-            gameObject.GetComponent<SpriteRenderer>().flipX = false;
-        }
-        if (!Input.GetKey("right") && !Input.GetKey("left"))
-        {
-            gameObject.GetComponent<Animator>().SetBool("moving", false);
-        }
+            gameObject.GetComponent<Animator>().SetBool("agachandose", false);
+            if (Input.GetKey("left"))
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(-7000f * Time.deltaTime, 0));
+                gameObject.GetComponent<Animator>().SetBool("moving", true);
+                gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
+            if (Input.GetKey("right"))
+            {
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(7000f * Time.deltaTime, 0));
+                gameObject.GetComponent<Animator>().SetBool("moving", true);
+                gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            }
+            if (!Input.GetKey("right") && !Input.GetKey("left"))
+            {
+                gameObject.GetComponent<Animator>().SetBool("moving", false);
+            }
 
-        if (Input.GetKeyDown("up") && canJump)
-        {
-            canJump = false;
-            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5000f));
+            if (Input.GetKeyDown("up") && canJump)
+            {
+                gameObject.GetComponent<Animator>().SetBool("jumping", true);
+                canJump = false;
+                gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 5000f));
+            }
+            if (!canJump)
+            {
+                gameObject.GetComponent<Animator>().SetBool("falling", true);
+                
+            }
         }
     }
 
@@ -39,7 +53,9 @@ public class PlayerControler : MonoBehaviour
     {
         if (collision.transform.tag == "Ground")
         {
+            gameObject.GetComponent<Animator>().SetBool("falling", false);
             canJump = true;
+            gameObject.GetComponent<Animator>().SetBool("jumping", false);
         }
     }
 }
