d6c838f70d2716d7b87bc9bed038f2d6074db6b3
ArielAraya <ariel.arayac@gmail.com>
2019-08-23 22:52:52 -0600

    First Commit

---
+++ PruebaPlataforma/Assets/Scripts/CamaraMov.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraMov : MonoBehaviour
+{
+    public GameObject personaje;
+    float posOriginal;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("right"))
+        {
+            gameObject.transform.Translate(((personaje.transform.position.x - posOriginal)+10) * Time.deltaTime, 0, 0);
+        }
+        posOriginal = personaje.transform.position.x;
+    }
+
+}
